9101786eeac1bf5c649a278169a165804e199300
inre.so <infra.so@gmail.com.com>
2024-11-10 16:35:09 +0200

    3rd commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud ITITITITI'); //write a response to the client
+  res.write('A Monk in Cloud ITITITITI22222222222222222222222'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
